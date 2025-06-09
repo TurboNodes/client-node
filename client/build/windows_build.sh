bf1a3fd32d625eac29373d545be4c002dd02c770
L1shed <skywarspanel@gmail.com>
2025-06-09 16:29:11 +0200

    CI/CD: update cross-platform client building

--- client/build/windows_build.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-GOOS=windows GOARCH=amd64 go build -ldflags="-H windowsgui" -o build/Turbo.exe
-
-if "%1"=="--installer" (
-    echo This would create a Windows installer with something like NSIS or InnoSetup
-)
